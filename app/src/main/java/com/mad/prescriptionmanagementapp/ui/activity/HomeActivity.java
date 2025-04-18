dfd4a21c4eecee26e9f06d6bfce7f0d79c22680a
VietHungHoang <viethunghoang1508@gmail.com>
2025-04-18 20:13:49 +0700

    Add dialog add options

--- app/src/main/java/com/mad/prescriptionmanagementapp/ui/activity/HomeActivity.java
+++ app/src/main/java/com/mad/prescriptionmanagementapp/ui/activity/HomeActivity.java
@@ -9,23 +9,66 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.mad.prescriptionmanagementapp.R;
+import com.mad.prescriptionmanagementapp.databinding.ActivityHomeBinding;
+import com.mad.prescriptionmanagementapp.ui.viewmodel.HomeViewModel;
+import com.mad.prescriptionmanagementapp.ui.viewmodel.LoginViewModel;
 
 public class HomeActivity extends AppCompatActivity {
+    private ActivityHomeBinding binding;
+    private HomeViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-        }
-        setContentView(R.layout.activity_home);
+        this.initParam();
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+    }
+
+    private void initParam() {
+        EdgeToEdge.enable(this);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+//        this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+
+        this.binding = ActivityHomeBinding.inflate(this.getLayoutInflater());
+        this.setContentView(this.binding.getRoot());
+        this.viewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+        this.binding.setViewModel(this.viewModel);
+        this.binding.setLifecycleOwner(this);
+
+        this.binding.fab.setOnClickListener(v -> showOptions());
+
+    }
+
+    private void showOptions() {
+        BottomSheetDialog dialog = new BottomSheetDialog(this);
+        View sheetView = getLayoutInflater().inflate(R.layout.dialog_add_options, null);
+
+        sheetView.findViewById(R.id.option_add_prescription).setOnClickListener(v -> {
+            viewModel.onAddPrescriptionClicked();
+            dialog.dismiss();
+        });
+
+        sheetView.findViewById(R.id.option_add_schedule).setOnClickListener(v -> {
+            viewModel.onAddScheduleClicked();
+            dialog.dismiss();
+        });
+
+        dialog.setContentView(sheetView);
+        dialog.show();
     }
 }
---
+++ app/src/main/java/com/mad/prescriptionmanagementapp/ui/viewmodel/HomeViewModel.java
@@ -0,0 +1,13 @@
+package com.mad.prescriptionmanagementapp.ui.viewmodel;
+
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+    public void onAddPrescriptionClicked() {
+        // TODO: Xử lý thêm đơn thuốc
+    }
+
+    public void onAddScheduleClicked() {
+        // TODO: Xử lý thêm lịch hẹn
+    }
+}
